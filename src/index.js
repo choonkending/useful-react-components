3e8e322a43cb49d269d07b55fba52de5ae959369
Simon Knox <psimyn@gmail.com>
2016-05-22 18:46:09 +1000

    add build script

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import { AppContainer } from 'react-hot-loader';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
 const rootEl = document.getElementById('root');
-ReactDOM.render(
-  <AppContainer>
-    <App />
-  </AppContainer>,
-  rootEl
-);
 
 if (module.hot) {
+  const AppContainer = require('react-hot-loader');
+  ReactDOM.render(
+    <AppContainer>
+      <App />
+    </AppContainer>,
+    rootEl);
   module.hot.accept('./App', () => {
     // If you use Webpack 2 in ES modules mode, you can
     // use <App /> here rather than require() a <NextApp />.
@@ -23,5 +22,7 @@ if (module.hot) {
       rootEl
     );
   });
+} else {
+  ReactDOM.render(<App />, rootEl);
 }
 
