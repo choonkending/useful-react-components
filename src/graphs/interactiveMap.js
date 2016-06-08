f70fac7ac830ad9c6f9062fe6663f49c2d383766
Ken Ding <aenkil@gmail.com>
2016-06-09 08:05:37 +1000

    Add drag events :v:

--- src/graphs/interactiveMap.js
+++ src/graphs/interactiveMap.js
@@ -5,11 +5,76 @@ const interactiveMap = ComposedComponent => class extends Component {
     super(props);
     this.state = {
       matrix: { a: 1, b: 0, c: 0, d: 1, e: 0, f: 0 },
-      dragging: false
+      dragging: false,
+      x: 0,
+      y: 0
     };
     this.pan = this.pan.bind(this);
     this.zoom = this.zoom.bind(this);
+    this.onDragStart = this.onDragStart.bind(this);
+    this.onDragMove = this.onDragMove.bind(this);
+    this.onDragEnd = this.onDragEnd.bind(this);
     this.onWheel = this.onWheel.bind(this);
+    this.getCoordinates = this.getCoordinates.bind(this);
+    this.isTouchEvent = this.isTouchEvent.bind(this);
+  }
+
+  render() {
+    const { matrix } = this.state;
+    const matrixValues = Object.keys(matrix).map(k => matrix[k]);
+    const { width, height, ...restProps } = this.props;
+    return (
+      <svg
+        width={width}
+        height={height}
+        onMouseDown={this.onDragStart}
+        onTouchStart={this.onDragStart}
+        onMouseMove={this.onDragMove}
+        onTouchMove={this.onDragMove}
+        onMouseUp={this.onDragEnd}
+        onTouchEnd={this.onDragEnd}
+        onWheel={this.onWheel}>
+        <g transform={`matrix(${matrixValues.join(' ')})`}>
+          <ComposedComponent {...restProps} />
+        </g>
+      </svg>
+    );
+  }
+
+  onWheel(e) {
+    if (e.deltaY < 0) {
+      this.zoom(1.05);
+    } else {
+      this.zoom(0.95);
+    }
+  }
+
+  onDragStart(e) {
+    const { x, y } = this.getCoordinates(e);
+    this.setState({
+      dragging: true,
+      x,
+      y
+    });
+  }
+
+  onDragMove(e) {
+    if (!this.state.dragging) return;
+    const { x, y } = this.getCoordinates(e);
+
+    // Take the delta where we are minus where we came from
+    const dx = x - this.state.x;
+    const dy = y - this.state.y;
+
+    this.pan(dx, dy);
+
+    // Update the new x and y position
+    // Because drag is likely a continuous movement
+    this.setState({ x, y});
+  }
+
+  onDragEnd() {
+    this.setState({ dragging: false });
   }
 
   pan(dx, dy) {
@@ -33,28 +98,16 @@ const interactiveMap = ComposedComponent => class extends Component {
     this.setState({ matrix: transformedMatrix });
   }
 
-  onWheel(e) {
-    if (e.deltaY < 0) {
-      this.zoom(1.05);
-    } else {
-      this.zoom(0.95);
-    }
+  isTouchEvent(e) {
+    return e.clientX === 'undefined';
   }
 
-  render() {
-    const { matrix } = this.state;
-    const matrixValues = Object.keys(matrix).map(k => matrix[k]);
-    const { width, height, ...restProps } = this.props;
-    return (
-      <svg
-        width={width}
-        height={height}
-        onWheel={this.onWheel}>
-        <g transform={`matrix(${matrixValues.join(' ')})`}>
-          <ComposedComponent {...restProps} />
-        </g>
-      </svg>
-    );
+  getCoordinates(e) {
+    const { clientX, clientY } = this.isTouchEvent(e) ? e.changedTouches[0] : e;
+    return {
+      x: clientX,
+      y: clientY
+    };
   }
 };
 
