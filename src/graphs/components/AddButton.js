74c592817e075f4004845c6c3b3eb11a1842edaa
Ken Ding <aenkil@gmail.com>
2016-06-14 18:45:11 +1000

    Fix correct attributes

--- src/graphs/components/AddButton.js
+++ src/graphs/components/AddButton.js
@@ -4,13 +4,11 @@ const AddButton = ({onClick, transformFn, bgColor, color, ...props}) => (
   <g onClick={onClick} transform={transformFn} {...props}>
     <circle cx="25" cy="25" r="25" fill={bgColor} />
     <line x1="25" x2="25" y1="13" y2="38"
-      fill="none" stroke={color} stroke-width="2"
-      stroke-linecap="round" stroke-linejoin="round"
-      stroke-miterlimit="10"/>
+      fill="none" stroke={color} strokeWidth="2"
+      strokeLinecap="round" />
     <line x1="13" x2="38" y1="25" y2="25"
-      fill="none" stroke={color} stroke-width="2"
-      stroke-linecap="round" stroke-linejoin="round"
-      stroke-miterlimit="10"/>
+      fill="none" stroke={color} strokeWidth="2"
+      strokeLinecap="round" />
   </g>
 );
 
