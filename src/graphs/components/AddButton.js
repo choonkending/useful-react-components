835eb180cf2d3df8247cd09a05d96e74f5cf8405
Ken Ding <aenkil@gmail.com>
2016-06-14 18:31:18 +1000

    Add defaultProps for buttton background color and color

--- src/graphs/components/AddButton.js
+++ src/graphs/components/AddButton.js
@@ -1,22 +1,23 @@
 import React from 'react';
 
-const AddButton = ({onClick, transformFn, ...props}) => (
+const AddButton = ({onClick, transformFn, bgColor, color, ...props}) => (
   <g onClick={onClick} transform={transformFn} {...props}>
-    <circle cx="25" cy="25" r="25" fill="#43B05C" />
+    <circle cx="25" cy="25" r="25" fill={bgColor} />
     <line x1="25" x2="25" y1="13" y2="38"
-      fill="none" stroke="#fff" stroke-width="2"
-      stroke-linecap="round" stroke-linejoin="round"
-      stroke-miterlimit="10"/>
-    <line x1="25" x2="25" y1="13" y2="38"
-      fill="none" stroke="#fff" stroke-width="2"
+      fill="none" stroke={color} stroke-width="2"
       stroke-linecap="round" stroke-linejoin="round"
       stroke-miterlimit="10"/>
     <line x1="13" x2="38" y1="25" y2="25"
-      fill="none" stroke="#fff" stroke-width="2"
+      fill="none" stroke={color} stroke-width="2"
       stroke-linecap="round" stroke-linejoin="round"
       stroke-miterlimit="10"/>
   </g>
 );
+
+AddButton.defaultProps = {
+  bgColor: "#42B05C",
+  color: "#fff"
+};
 
 export default AddButton;
 
