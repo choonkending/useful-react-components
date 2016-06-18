e3e7b50ed424bd909f4a805e41e6454d4a24a276
Ken Ding <aenkil@gmail.com>
2016-06-18 13:54:28 +1000

    add onClose handler to zoomout

--- src/graphs/components/Node.js
+++ src/graphs/components/Node.js
@@ -4,19 +4,26 @@ export default class Node extends Component {
   constructor(props) {
     super(props);
     this.onOpen = this.onOpen.bind(this);
+    this.onClose = this.onClose.bind(this);
   }
 
   render() {
-    const { cx, cy, r, bgColor } = this.props;
+    const { cx, cy, r, isActive, bgColor} = this.props;
     return (
-      <circle onClick={this.onOpen} r={r} cx={cx} cy={cy} fill={bgColor} />
+      <g>
+        <circle onClick={this.onOpen} r={r} cx={cx} cy={cy} fill={bgColor} />
+        { isActive && <circle onClick={this.onClose} r={r} cx={cx + 100 } cy={cy} fill="#42B05C" /> }
+      </g>
     );
   }
 
   onOpen() {
     const { index, onOpen } = this.props;
-
     onOpen && onOpen(index);
+  }
+
+  onClose() {
+    this.props.onClose && this.props.onClose();
   }
 }
 
@@ -24,7 +31,9 @@ Node.propTypes = {
   index: PropTypes.number.isRequired,
   r: PropTypes.number.isRequired,
   cx: PropTypes.number.isRequired,
-  cy: PropTypes.number.isRequired
+  cy: PropTypes.number.isRequired,
+  bgColor: PropTypes.string.isRequired,
+  onOpen: PropTypes.func
 };
 
 export default Node;
