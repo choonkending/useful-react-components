bd245f68ddf791bd54e05a48ea72a28a365aa2b9
Ken Ding <aenkil@gmail.com>
2016-06-20 23:16:10 +1000

    Position Add Button at bottom

--- src/graphs/FinalMap/FinalMap.js
+++ src/graphs/FinalMap/FinalMap.js
@@ -4,16 +4,26 @@ import AddButton from '../components/AddButton';
 class FinalMap extends Component {
   constructor(props) {
     super(props);
+    this.getViewBoxValues = this.getViewBoxValues.bind(this);
+    this.state = { viewBox: [] };
   }
 
   render() {
+    const { width, height, top } = this.props;
+    const viewBox = this.getViewBoxValues();
     return (
-      <svg className="final">
-        <AddButton className="map__button" />
+      <svg viewBox={viewBox}>
+      <AddButton x={width/2} y={height - top - 50} className="map__button" />
       </svg>
     );
   }
 
+  getViewBoxValues() {
+    const { width, height } = this.props;
+    const { viewBox } = this.state;
+    if (width == null || height == null) return;
+    return viewBox.length > 0 ? viewBox.join(' ') : `0 0 ${width} ${height}`;
+  }
 }
 
 export default FinalMap;
